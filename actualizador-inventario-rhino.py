08e6f6e12d0d816d5b3a2f8f1417f144faf68c8e
Víctor Olivares/ Rhino <volivares@rhino.mx>
2026-06-26 09:53:32 -0600

    Se realizó funcionalidad de actualizar inventario en página de básculas electrónicas

--- actualizador-inventario-rhino.py
+++ actualizador-inventario-rhino.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.set_page_config(page_title="Rhino Inventarios", page_icon="🦏")
-st.title("Actualizador de Inventario")
+st.title("Actualizador de Inventario Página Rhino")
 
 st.markdown("Carga de Archivos")
 
@@ -47,4 +47,50 @@ if file1 and file2:
         file_name='wc-product-ACTUALIZADO.csv',
     )
 
-    st.markdown('Que la fuerza los acompañe')
+st.title("Actualizador de Inventario Básculas Electrónicas")
+
+st.markdown("Carga de Archivos")
+
+# Subir archivos
+file3 = st.file_uploader(
+    "Arrastra el archivo CSV exportado de la página Básculas electrónicas", type=['csv'])
+file4 = st.file_uploader(
+    "Arrastra el archivo archivo excel del inventario diario", type=['xlsx', 'xls'])
+
+if file3 and file4:
+    # leer datos
+    df3 = pd.read_csv(file1)
+    df4 = pd.read_excel(file2)
+    # Procesamiento
+
+    # Extraer skus en Backorder
+    df_backorder = df4[df4['TIPO DE ABASTO'] == 'BACKORDER']
+    skus_backorder = df_backorder['MODELO']
+    # Devolver todo el inventario a 999
+    df3['Available (not editable)'] = 999
+    df3['On hand (current)'] = 999
+    # Cambiar a 0 los skus en backorder
+    df3.loc[df3['SKU'].isin(skus_backorder), 'Available (not editable)'] = 0
+
+    # Resumen para el usuario
+    st.success("Archivos procesados correctamente")
+    st.balloons()
+    col1, col2 = st.columns(2)
+    col1.metric("Encontrados en Backorder", len(skus_backorder))
+
+    with st.expander("Ver Resultado"):
+        st.dataframe(df1[df1['¿En inventario?'] == 0]
+                     [['SKU', '¿En inventario?']])
+
+    # Creación y codificación de archivo nuevo
+    csv_data = df3.to_csv(index=False, encoding='utf-8-sig')
+    csv_resultado = csv_data.encode('utf-8-sig')
+
+    # Descargar Archivo nuevo
+    st.download_button(
+        label='Descargar Inventario Actualizado',
+        data=csv_resultado,
+        file_name='wc-product-ACTUALIZADO.csv',
+    )
+
+    st.title("Soy uno con la fuerza, la fuerza está conmigo")
